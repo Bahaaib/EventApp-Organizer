ec61ad8cf946ddfd117cdb9e9a9b54a88fdd046b
Bahaaib <bahaasco@gmail.com>
2019-10-09 09:26:51 +0200

    added location fragment design

--- app/src/main/java/com/bahaa/eventorganizerapp/Fragments/MoreFragment.java
+++ app/src/main/java/com/bahaa/eventorganizerapp/Fragments/MoreFragment.java
@@ -2,16 +2,24 @@ package com.bahaa.eventorganizerapp.Fragments;
 
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.bahaa.eventorganizerapp.R;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
+
 public class MoreFragment extends Fragment {
 
+    Unbinder unbinder;
 
     public MoreFragment() {
         // Required empty public constructor
@@ -22,7 +30,22 @@ public class MoreFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_more, container, false);
+        View v = inflater.inflate(R.layout.fragment_more, container, false);
+
+        unbinder = ButterKnife.bind(this, v);
+
+        return v;
+    }
+
+    @OnClick(R.id.more_event_next)
+    void launchMoreInfoFragment(){
+        Log.i("statuss", "clicked!");
+        LocationFragment locationFragment = new LocationFragment();
+        FragmentManager manager = getFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.fragment_container, locationFragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 
 }
